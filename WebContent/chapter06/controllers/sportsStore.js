169a9d152ca79dcce8827d8b4dfc6e6af05d61a4
Hai Tang <402696415@qq.com>
2016-05-30 13:52:57 -0500

    May 30th

--- WebContent/chapter06/controllers/sportsStore.js
+++ WebContent/chapter06/controllers/sportsStore.js
@@ -33,16 +33,23 @@
 //	});
 
 angular.module("sportsStore")
+.constant("dataUrl","http://localhost:8080/products")
 .controller("sportsStoreCtrl",function($scope){
-	$scope.data = {
-		products:[
-	          { name: "Product #1",description:"A product",
-	        	  category:"Category #1",price:100},
-        	  { name: "Product #2",description:"A product",
-	        	  category:"Category #1",price:110},
-        	  { name: "Product #3",description:"A product",
-	        	  category:"Category #2",price:210},
-        	  { name: "Product #4",description:"A product",
-	        	  category:"Category #3",price:202}]
-	};
+	$scope.data = {};
+	
+	$http.get(dataUrl)
+		.success(function(data){
+			$scope.data.products = data;
+		})
+		.error
+//		products:[
+//	          { name: "Product #1",description:"A product",
+//	        	  category:"Category #1",price:100},
+//        	  { name: "Product #2",description:"A product",
+//	        	  category:"Category #1",price:110},
+//        	  { name: "Product #3",description:"A product",
+//	        	  category:"Category #2",price:210},
+//        	  { name: "Product #4",description:"A product",
+//	        	  category:"Category #3",price:202}]
+//	};
 });
