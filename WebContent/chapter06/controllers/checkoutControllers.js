f1f19018a21da0ed6893232016625541056ec707
Hai Tang <402696415@qq.com>
2016-06-04 15:23:05 -0500

    HBD

--- WebContent/chapter06/controllers/checkoutControllers.js
+++ WebContent/chapter06/controllers/checkoutControllers.js
@@ -6,7 +6,12 @@ angular.module("sportsStore")
 	$scope.total = function(){
 		var total = 0;
 		for(var i = 0; i < $scope.cartData.length; i++){
-			
+			total += ($scope.cartData[i].price * $scope.cartData[i].count);	
 		}
+		return total;
 	}
-})
+	
+	$scope.remove = function(id){
+		cart.removeProduct(id);
+	}
+});
