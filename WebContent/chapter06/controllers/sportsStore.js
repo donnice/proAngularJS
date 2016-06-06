1084a1a682b9f3f20956332abf90a0fcabe92f9c
Hai Tang <402696415@qq.com>
2016-06-05 21:09:38 -0500

    Jun 5th

--- WebContent/chapter06/controllers/sportsStore.js
+++ WebContent/chapter06/controllers/sportsStore.js
@@ -47,6 +47,12 @@ angular.module("sportsStore")
 		});
 	
 	$scope.sendOrder = function(shippingDetails){
-		
+		var order = angular.copy(shippingDetails);
+		order.products = cart.getProducts();
+		$http.post(orderUrl, order)
+		.success(function(data){
+			$scope.data.orderId = data.id;
+			cart.getProducts().length = 0;
+		})
 	}
 });
