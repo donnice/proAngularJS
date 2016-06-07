11df6b36b5bc7970fd4215105acc9bba9e6aa386
Hai Tang <402696415@qq.com>
2016-06-06 23:09:56 -0500

    Jun 6th

--- WebContent/chapter06/controllers/adminControllers.js
+++ WebContent/chapter06/controllers/adminControllers.js
@@ -1,5 +1,6 @@
 angular.module('sportsStoreAdmin')
 	.constant('authUrl','http://localhost:8080/proAngularJS/chapter06/users/login')
+	.constant("ordersUrl","http://localhost:8080/proAngularJS/chapter06/orders")
 	.controller('authCtrl',function($scope,$http,$location,authUrl){
 		$scope.authenticate = function(user,pass){
 			$http.post(authUrl,{
@@ -28,9 +29,26 @@ angular.module('sportsStoreAdmin')
 		}
 	})
 	.controller('ordersCtrl',function($scope,$http,ordersUrl){
-		
-	})$http.get(ordersUrl,{withCredentials:true})
+		$http.get(ordersUrl,{withCredentials:true})
 		.success(function(data){
-			
+			$scope.orders = data;
 		})
+		.error(function(error){
+			$scope.error = error;
+		});
+		
+		$scope.selectedOrder;
+		
+		$scope.selectOrder = function(order){
+			$scope.selectedOrder = order;
+		};
+		
+		$scope.calcTotal = function(order){
+			var total = 0;
+			for(var i = 0; i < order.products.length; i++){
+				total += order.products[i].count * order.products[i].price;
+			}
+			return total;
+		}
+	});
 		
