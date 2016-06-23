f64ff65b8b1f7264b7be11eec920119ff6206aff
Hai Tang <402696415@qq.com>
2016-06-22 23:14:01 -0500

    Jun 22nd

---
+++ WebContent/chapter13/controllers.js
@@ -0,0 +1,26 @@
+var app = angular.module("exampleApp",[]);
+
+app.controller("topLevelCtrl",function($scope){
+	
+	$scope.dataValue = "Hello,Adam";
+	
+	$scope.reverseText = function(){
+		$scope.dataValue = $scope.dataValue.split("").reverse().join("");
+	}
+	
+	$scope.changeCase = function(){
+		var result = [];
+		angular.forEach($scope.dataValue.split(""),function(char,index){
+			result.push(index%2 == 1
+				? char.toString().toUpperCase():char.toString().toLowerCase());
+		});
+		$scope.dataValue=result.join("");
+	};
+});
+
+app.controller("firstChildCtrl",function($scope){
+	$scope.changeCase = function(){
+		$scope.dataValue = $scope.dataValue.toUpperCase();
+	};
+});
+
