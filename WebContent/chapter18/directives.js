1fe500d66238ee49cb498a19c6751375411c55a5
Hai Tang <402696415@qq.com>
2016-07-07 21:54:03 -0500

    Jul 7th

---
+++ WebContent/chapter18/directives.js
@@ -0,0 +1,20 @@
+//angular.module("exampleApp")
+angular.module("customDirectives",[])
+.directive("triButton",function(){
+	return { 
+		// @ allows a value defined on the directive attribute to be passed to the directive's isolate scope. 
+		// & allows the directive's isolate scope to pass values into the parent scope for evaluation in the
+		//			expression defined in the attribute. 
+		// = sets up a two-way binding expression between the directive's isolate scope and the parent scope.
+		
+		scope:{counter:"=counter"},
+		link: function(scope,element,attrs){
+			element.on("click",function(event){
+				console.log("Button click: "+event.target.innerText);
+				scope.$apply(function(){
+					scope.counter++;
+				});
+			});
+		}		
+	}
+});
