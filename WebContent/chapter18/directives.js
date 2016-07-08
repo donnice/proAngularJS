b22fcb6cb2c2c4d502cb801e0757737ee7f5cbc0
Hai Tang <402696415@qq.com>
2016-07-07 23:45:24 -0500

    Jul 7th

--- WebContent/chapter18/directives.js
+++ WebContent/chapter18/directives.js
@@ -1,6 +1,6 @@
 //angular.module("exampleApp")
 angular.module("customDirectives",["customServices"])
-.directive("triButton",function(){
+.directive("triButton",function(logService){
 	return { 
 		// @ allows a value defined on the directive attribute to be passed to the directive's isolate scope. 
 		// & allows the directive's isolate scope to pass values into the parent scope for evaluation in the
@@ -10,7 +10,7 @@ angular.module("customDirectives",["customServices"])
 		scope:{counter:"=counter"},
 		link: function(scope,element,attrs){
 			element.on("click",function(event){
-				//logService.log("Button click: "+event.target.innerText);
+				logService.log("Button click: "+event.target.innerText);
 				scope.$apply(function(){
 					scope.counter++;
 				});
