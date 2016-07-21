a0dfb3be74bec9687ab123afbf76c198ee0f1c82
Hai Tang <402696415@qq.com>
2016-07-20 22:02:23 -0500

    increment.js

--- WebContent/chapter21/increment.js
+++ WebContent/chapter21/increment.js
@@ -1,1 +1,23 @@
 angular.module("increment",[])
+	.directive("increment",function(){
+//		'A' - only matches attribute name <div attribute>
+//		'E' - only matches element name	<element></element>
+//		'C' - only matches class name
+//		'M' - only matches comment
+		return {
+			restrict:"E",
+			scope:{
+				 value:"=value"
+			},
+			link:function(scope,element,attrs){
+				var button = angular.element("<button>").text("+");
+				button.addClass("btn btn-primary btn-xs");
+				element.append(button);
+				button.on("click",function(){
+					scope.$apply(function(){
+						scope.value++;
+					})
+				})
+			},
+		}
+	});
