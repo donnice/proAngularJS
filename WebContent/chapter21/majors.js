6cb43eb918ab01c2cc893b5f09d0af07ebf0a20a
Hai Tang <402696415@qq.com>
2016-07-20 21:49:49 -0500

    JSON again

--- WebContent/chapter21/majors.js
+++ WebContent/chapter21/majors.js
@@ -1,24 +1,28 @@
 angular.module("exampleApp",[])
-.controller("defaultCtrl",function($scope){
+.constant("baseUrl","http://localhost:8080/proAngularJS/chapter21/majorData.json")
+.controller("defaultCtrl",function($scope,$http,baseUrl){
 	
 	$scope.displayMode = "list";
 	$scope.currentMajor = null;
 	
 	$scope.listMajors = function() {
-		$scope.majors = [
-		    {id:0, name:"East China University of Science of Technology", category:"Environmental Engineering", price:10000},
-		    {id:1, name:"University of Michigan at Ann Arbor", category:"Chemical Engineering", price:66000},
-		    {id:2, name:"Georgia Institute of Technology", category:"Computer Science", price:6000},
-		    {id:3, name:"University of Washington at Seattle", category:"Computational Finance and Risk Management", price:37800},
-		    {id:4, name:"University of California at Los Angeles", category:"Electrical Engineering", price:62000},
-		    {id:5, name:"California Institute of Technology", category:"Applied Physics and Material Science", price:162000},
-		    {id:6, name:"Massachusets Institute of Technology", category:"Mechanical Engineering", price:30000},
-		    {id:7, name:"Karlsruhe Institute of Technology", category:"Biology", price:3336},
-		    {id:8, name:"The Hong Kong University of Science and Technology", category:"Civil Engineering", price:15000},
-		    {id:9, name:"The Chinese University of Hong Kong", category:"Juris Doctor", price:12885},
-		    {id:10, name:"The University of Hong Kong", category:"Medicine", price:18811}
-		    
-		];
+//		$scope.majors = [
+//		    {id:0, name:"East China University of Science of Technology", category:"Environmental Engineering", price:10000},
+//		    {id:1, name:"University of Michigan at Ann Arbor", category:"Chemical Engineering", price:66000},
+//		    {id:2, name:"Georgia Institute of Technology", category:"Computer Science", price:6000},
+//		    {id:3, name:"University of Washington at Seattle", category:"Computational Finance and Risk Management", price:37800},
+//		    {id:4, name:"University of California at Los Angeles", category:"Electrical Engineering", price:62000},
+//		    {id:5, name:"California Institute of Technology", category:"Applied Physics and Material Science", price:162000},
+//		    {id:6, name:"Massachusets Institute of Technology", category:"Mechanical Engineering", price:30000},
+//		    {id:7, name:"Karlsruhe Institute of Technology", category:"Biology", price:3336},
+//		    {id:8, name:"The Hong Kong University of Science and Technology", category:"Civil Engineering", price:15000},
+//		    {id:9, name:"The Chinese University of Hong Kong", category:"Juris Doctor", price:12885},
+//		    {id:10, name:"The University of Hong Kong", category:"Medicine", price:18811}
+//		    
+//		];
+		$http.get(baseUrl).success(function(data){
+			$scope.majors = data;
+		})
 	}
 	
 	$scope.deleteMajor = function(major) {
