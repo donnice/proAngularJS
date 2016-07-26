db138cdabd7c983ff1cb872c27dc5c59e31ca746
Hai Tang <402696415@qq.com>
2016-07-26 09:10:37 -0500

    test

---
+++ WebContent/chapter25/firstTest.js
@@ -0,0 +1,23 @@
+// page 20
+describe("First Test",function(){
+	
+	var counter;
+	
+	beforeEach(function(){
+		counter = 0;
+	});
+	
+	it("increments value", function(){
+		counter++;
+		
+		// Assert
+		expect(counter).toEqual(1);
+	})
+	
+	it("decrements value", function(){
+		counter--;
+		
+		// Assert
+		expect(counter).toEqual(0);
+	})
+});
