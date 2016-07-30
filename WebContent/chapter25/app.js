e47d11852b83ed7a5a6fec6049e092a6e5f4ec7c
Hai Tang <402696415@qq.com>
2016-07-30 17:13:56 -0500

    service test and last commit T_T

--- WebContent/chapter25/app.js
+++ WebContent/chapter25/app.js
@@ -44,4 +44,15 @@ angular.module("exampleApp",[])
 				}
 			}
 		}
+	})
+	.factory("counterService",function(){
+		var counter = 0;
+		return {
+			incrementCounter:function(){
+				counter++;
+			},
+			getCounter:function(){
+				return counter;
+			}
+		}
 	});
